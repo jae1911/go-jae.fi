dc14948156393f594e958dd0b0ebeed14686f879
Jae Lo Presti (DN0) <me@jae.fi>
2022-01-10 23:06:20 +0200

    Main: convert tabs to spaces

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
-	"os"
-	"log"
-	"html/template"
-	"errors"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "log"
+    "html/template"
+    "errors"
 
-	"github.com/gin-contrib/gzip"
-	"github.com/gin-gonic/gin"
-	"github.com/russross/blackfriday"
+    "github.com/gin-contrib/gzip"
+    "github.com/gin-gonic/gin"
+    "github.com/russross/blackfriday"
 )
 
 // For content pages
@@ -20,108 +20,108 @@ type Post struct {
 }
 
 func main() {
-	// Web service
-	r := gin.New()
-	r.Use(gin.Recovery())
+    // Web service
+    r := gin.New()
+    r.Use(gin.Recovery())
 
-	// Streaming
-	stream_online := false
+    // Streaming
+    stream_online := false
 
-	// Compression
-	r.Use(gzip.Gzip(gzip.DefaultCompression))
+    // Compression
+    r.Use(gzip.Gzip(gzip.DefaultCompression))
 
-	r.Delims("{{", "}}")
+    r.Delims("{{", "}}")
 
-	// Personal info endpoint
-	r.GET("/me", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"name":       "Jae",
-			"familyname": "Lo Presti",
-			"job":        "Back-end Developer",
-			"birthday":   "2001-04-28",
-			"location":   "Helsinki, Finland, Europe, Earth, Alpha Quadrant",
-			"email":      "me@jae.fi",
-			"matrix":     "@me:jae.fi",
-			"fediverse":  "@jae@mastodon.tedomum.net",
-			"pronouns":   "She/Her",
-			"stream_online": stream_online,
-		})
-	})
+    // Personal info endpoint
+    r.GET("/me", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "name":       "Jae",
+            "familyname": "Lo Presti",
+            "job":        "Back-end Developer",
+            "birthday":   "2001-04-28",
+            "location":   "Helsinki, Finland, Europe, Earth, Alpha Quadrant",
+            "email":      "me@jae.fi",
+            "matrix":     "@me:jae.fi",
+            "fediverse":  "@jae@mastodon.tedomum.net",
+            "pronouns":   "She/Her",
+            "stream_online": stream_online,
+        })
+    })
 
-	// Webring members endpoint
-	// TODO: use a database instead of a plain file
-	r.GET("/webring/members", func(c *gin.Context) {
-		jsonfile, err := ioutil.ReadFile("./webring/members.json")
-		if err != nil {
-			c.JSON(500, gin.H{
-				"error": "Failed to read members",
-			})
-			return
-		}
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+    // Webring members endpoint
+    // TODO: use a database instead of a plain file
+    r.GET("/webring/members", func(c *gin.Context) {
+        jsonfile, err := ioutil.ReadFile("./webring/members.json")
+        if err != nil {
+            c.JSON(500, gin.H{
+                "error": "Failed to read members",
+            })
+            return
+        }
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "false")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
 
-		c.Data(http.StatusOK, "application/json", jsonfile)
-	})
+        c.Data(http.StatusOK, "application/json", jsonfile)
+    })
 
-	// Matrix server delegation
-	r.GET("/.well-known/matrix/server", func(c *gin.Context) {
-		c.JSON(200, gin.H {
-			"m.server": "matrix.jae.fi:443",
-		})
-	})
+    // Matrix server delegation
+    r.GET("/.well-known/matrix/server", func(c *gin.Context) {
+        c.JSON(200, gin.H {
+            "m.server": "matrix.jae.fi:443",
+        })
+    })
 
-	// Static routes & templates
-	r.LoadHTMLGlob("templates/**/*.tmpl")
-	r.Static("/assets", "./static")
+    // Static routes & templates
+    r.LoadHTMLGlob("templates/**/*.tmpl")
+    r.Static("/assets", "./static")
 
-	// Main page, cannot be handled by :page
-	r.GET("/", func(c *gin.Context) {
-		postContent, err := ioutil.ReadFile("content/index.md")
-		if err != nil {
-			log.Fatal(err)
-		}
+    // Main page, cannot be handled by :page
+    r.GET("/", func(c *gin.Context) {
+        postContent, err := ioutil.ReadFile("content/index.md")
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		postHTML := template.HTML(blackfriday.MarkdownCommon([]byte(postContent)))
+        postHTML := template.HTML(blackfriday.MarkdownCommon([]byte(postContent)))
 
-		post := Post{Title: "Main Page", Content: postHTML}
+        post := Post{Title: "Main Page", Content: postHTML}
 
-		c.HTML(http.StatusOK, "globals/complete.tmpl", gin.H{
-			"title":        post.Title,
-			"content":		post.Content,
-			"currentsite":  "Jae's Website",
-			"stream_online": stream_online,
-		})
-	})
+        c.HTML(http.StatusOK, "globals/complete.tmpl", gin.H{
+            "title":        post.Title,
+            "content":      post.Content,
+            "currentsite":  "Jae's Website",
+            "stream_online": stream_online,
+        })
+    })
 
-	// Get routes
-	r.GET("/:page", func(c *gin.Context) {
-		requestedPage := c.Param("page")
-		contentLocation := "content/" + requestedPage + ".md"
+    // Get routes
+    r.GET("/:page", func(c *gin.Context) {
+        requestedPage := c.Param("page")
+        contentLocation := "content/" + requestedPage + ".md"
 
-		// Check if file exists
-		if _, err := os.Stat("content/" + requestedPage + ".md"); errors.Is(err, os.ErrNotExist){
-			contentLocation = "content/404.md"
-		}
+        // Check if file exists
+        if _, err := os.Stat("content/" + requestedPage + ".md"); errors.Is(err, os.ErrNotExist){
+            contentLocation = "content/404.md"
+        }
 
-		postContent, err := ioutil.ReadFile(contentLocation)
-		if err != nil {
-			log.Fatal(err)
-		}
-		
-		postHTML := template.HTML(blackfriday.MarkdownCommon([]byte(postContent)))
+        postContent, err := ioutil.ReadFile(contentLocation)
+        if err != nil {
+            log.Fatal(err)
+        }
+        
+        postHTML := template.HTML(blackfriday.MarkdownCommon([]byte(postContent)))
 
-		post := Post{Title: requestedPage, Content: postHTML}
+        post := Post{Title: requestedPage, Content: postHTML}
 
-		c.HTML(http.StatusOK, "globals/complete.tmpl", gin.H{
-			"title": post.Title,
-			"content": post.Content,
-			"stream_online": stream_online,
-		})
+        c.HTML(http.StatusOK, "globals/complete.tmpl", gin.H{
+            "title": post.Title,
+            "content": post.Content,
+            "stream_online": stream_online,
+        })
 
-	})
+    })
 
-	r.Run(":2021")
+    r.Run(":2021")
 }
